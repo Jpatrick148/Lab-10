d1ad34b985d35ae354bdee3517f4177e8a8b1eab
Joseph <Jpatrick148@Gmail.com>
2018-10-17 15:46:49 -0400

    Complete

---
+++ Circle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10
+{
+    class Circle
+    {
+        private double radius, c, area, a;
+        private string formatC;
+        private string formatA, formatN;
+
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        //private string FormatNumber(double x)
+        //{
+
+        //    double radius = string.Format("{0:0.00}", x);
+        //    return formatN;
+        //}
+
+        public double CalculateCircumference()
+        {
+            c = radius * 2 * Math.PI;
+            return c;
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+            c = CalculateCircumference();
+            formatC = string.Format("The Circumference of your circle is  {0:0.00}", c);
+            return formatC;
+        }
+
+        public double CalculateArea()
+        {
+            a = (Math.PI * radius * radius);
+            return a;
+        }
+
+        public string CalculateFormattedArea()
+        {
+            a = CalculateArea();
+            formatA = string.Format("The Area of your circle is {0:0.00}", a);
+            return formatA;
+        }
+
+      
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            List<Circle> circles = new List<Circle>();
+
+
+            double userNum;
+            bool check;
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Hello User! Please enter a Radius. ");
+                    string input = Console.ReadLine();
+                    check = double.TryParse(input, out userNum);
+                } while (!check);
+
+                Circle c1 = new Circle(userNum);
+                circles.Add(c1);
+
+
+                Console.WriteLine(c1.CalculateFormattedCircumference());
+                Console.WriteLine(c1.CalculateFormattedArea());
+            } while (Continue());
+            Console.WriteLine("You've created " + circles.Count + " Circles.");
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+
+        public static bool Continue()
+        {
+            Console.WriteLine("Again? {Y/N}");
+            string input = Console.ReadLine().ToLower();
+            if (input == "y")
+            {
+                return true;
+            }
+            else if (input == "n")
+            {
+                return false;
+            }
+            else Console.WriteLine("Please Try Again.");
+            return Continue();
+        }
+    }
+}
